1097af25b889da71404de37199c9fa770765028b
Udaya kumar <udayainfo@gmail.com>
2014-03-19 12:14:53 +0530

    Create UIApplication+ApplicationScreenSize.m

---
+++ UIApplication+ApplicationScreenSize.m
@@ -0,0 +1,31 @@
+//
+//  UIApplication+ApplicationScreenSize.m
+//
+
+#import "UIApplication+ApplicationScreenSize.h"
+
+
+@implementation UIApplication (ApplicationScreenSize)
+
++(CGSize) currentScreenSize
+{
+    return [UIApplication sizeInOrientation:[UIApplication sharedApplication].statusBarOrientation];
+}
+
++(CGSize) sizeInOrientation:(UIInterfaceOrientation)orientation
+{
+    CGSize size = [UIScreen mainScreen].bounds.size;
+    UIApplication *application = [UIApplication sharedApplication];
+    if (UIInterfaceOrientationIsLandscape(orientation))
+    {
+        size = CGSizeMake(size.height, size.width);
+    }
+    if (application.statusBarHidden == NO)
+    {
+        size.height -= MIN(application.statusBarFrame.size.width, application.statusBarFrame.size.height);
+    }
+    return size;
+}
+
+
+@end
